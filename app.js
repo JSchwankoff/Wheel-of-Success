1880af28a54e307e70ee3868750669cfb8346f98
James Schwankoff <j.a.schwankoff@gmail.com>
2022-03-30 22:58:57 -0400

    display phrase func, check guess func, key listener, lifes, win func

--- app.js
+++ app.js
@@ -1,36 +1,81 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
+const ul = phrase.querySelector('ul');
+let tries = document.querySelectorAll('li[class=tries]');
 let missed = 0;
 
-const startBtn = document.querySelector('.btn__reset');
+const btnMain = document.querySelector('.btn__reset');
 const overlay = document.getElementById('overlay');
-startBtn.addEventListener('click', () => {
+btnMain.addEventListener('click', () => {
   overlay.style.display = 'none';
 });
 
 const phrases = [
-  'I love going outside when it is raining',
-  'Dogs are my favorite kind of animal',
-  'Jupiter is the largest planet in the galaxy',
-  'Spiders spin webs to catch their prey',
-  'Javascript makes websites more interactive'
+  'i love going outside when it is raining',
+  'dogs are my favorite kind of animal',
+  'jupiter is the largest planet in the galaxy',
+  'spiders spin webs to catch their prey',
+  'javascript makes websites more interactive'
 ]
 
 function getRandomPhraseAsArray(array) {
   let phrase = array[Math.floor(Math.random() * array.length)];
-  let phraseCharacters = [...phrase];
+  var phraseCharacters = [...phrase];
   return phraseCharacters;
 }
 
+let characters = getRandomPhraseAsArray(phrases);
+
 function addPhraseToDisplay(array) {
-  const ul = phrase.querySelector('ul');
   for (let i = 0; i < array.length; i++) {
     const characterLI = document.createElement('li');
     characterLI.textContent = array[i];
-    if (characterLI.textContent === /[a-zA-Z]/) {
+    if (characterLI.textContent !== ' ') {
       characterLI.className = 'letter';
     }
     ul.appendChild(characterLI);
   }
 }
-// Put getRandomPhraseAsArray into add addPhraseToDisplay??
+
+addPhraseToDisplay(characters);
+
+function checkLetter(button) {
+  let letters = ul.querySelectorAll('li[class=letter]');
+  let btnChosen = button.textContent;
+  let correctLetter = null;
+  for (let i = 0; i < letters.length; i++) {
+    let letter = letters[i].textContent;
+    if (btnChosen === letter) {
+      letters[i].className += ' show';
+      correctLetter = letter;
+    }
+  }
+  return correctLetter;
+}
+
+qwerty.addEventListener('click', (e) => {
+  let guess = e.target;
+  guess.className = 'chosen';
+  let letterFound = checkLetter(guess);
+  if (letterFound === null) {
+    missed += 1;
+    tries[missed - 1].firstChild.src = 'images/lostHeart.png';
+  }
+  checkWin();
+});
+
+function checkWin() {
+  const shown = ul.querySelectorAll('li[class=show]');
+  const totalLetters = ul.querySelectorAll('li[class=letter]');
+  const gameOver = document.querySelector('.title');
+  if (shown.length === totalLetters.length) {
+    overlay.className = 'win';
+    gameOver.textContent = 'You Win!!';
+    overlay.style.display = 'inherit';
+  } else if (missed >= 5) {
+    overlay.className = 'lose';
+    gameOver.textContent = 'You Lose';
+    overlay.style.display = 'inherit';
+  }
+  btnMain.textContent = 'Reset';
+}
